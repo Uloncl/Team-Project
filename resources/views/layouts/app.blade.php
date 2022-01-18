f5b531cf2f3c94fb795aa9865fb086c0dc1d90cf
Nebula6 <eaglenebula286@gmail.com>
2022-01-18 13:48:56 +0000

    added app as a template for all pages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name', 'Team Project')  }}</title>
+    </head>
+    <body>
+    </body>
+</html>
