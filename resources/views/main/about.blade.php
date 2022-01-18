2efcf11791c455c11fc410674aabd29e55f71e4e
Nebula6 <eaglenebula286@gmail.com>
2022-01-18 12:38:41 +0000

    title uses .env

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>About</title>
+        <title>{{ config('app.name', 'Team Project')  }}</title>
     </head>
     <body>
     </body>
