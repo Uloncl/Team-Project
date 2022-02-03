9165d89c3627ed847128dc89f533819fcebe1fbd
Nebula6 <eaglenebula286@gmail.com>
2022-02-03 12:12:40 +0000

    routes for each person

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,7 +13,19 @@ class PagesController extends Controller
 	public function about() {
 		return view('main.about');
 	}
-	public function projects() {
-		return view('main.projects');
+	public function projects($name) {
+		if ($name == null){
+			return view('main.projects');
+		} elseif ($name == "lewis") {
+			return view('lewis.Lewis');
+		} elseif ($name == "andrea") {
+			return view('andrea.andrea');
+		} elseif ($name == "ben") {
+			return view('ben.ben');
+		} elseif ($name == "aryan") {
+			return view('aryan.aryan');
+		} elseif ($name == "mabel") {
+			return view('mabel.mabel');
+		}
 	}
 }
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,25 +3,44 @@
 		<a class="navbar-brand" href="{{ url('/') }}">
 			{{ config('app.name', 'Laravel') }}
 		</a>
-		<a class="navbar-brand" href="{{ url('/about') }}">
-			About
-		</a>
-		<a class="navbar-brand" href="{{ url('/projects') }}">
-			Projects
-		</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
+		<div class="collapse navbar-collapse" id="navbarsExample07">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a class="navbar-brand" href="{{ url('/about') }}">
+						About
+					</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="{{ url('/projects') }}" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Projects</a>
+					<ul class="dropdown-menu" aria-labelledby="dropdown07">
+						<li><a class="dropdown-item" href="{{ url('/projects/lewis') }}">Lewis</a></li>
+						<li><a class="dropdown-item" href="{{ url('/projects/andrea') }}">Andrea</a></li>
+						<li><a class="dropdown-item" href="{{ url('/projects/ben') }}">Ben</a></li>
+						<li><a class="dropdown-item" href="{{ url('/projects/aryan') }}">Aryan</a></li>
+						<li><a class="dropdown-item" href="{{ url('/projects/mabel') }}">Mabel</a></li>
+					</ul>
+				</li>
+			</ul>
+			<form>
+				<input class="form-control" type="text" placeholder="Search" aria-label="Search">
+			</form>
+		</div>
+	</div>
+	<!-- <div class="container">
+
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<!-- Left Side Of Navbar -->
+			Left Side Of Navbar
 			<ul class="navbar-nav me-auto">
 
 			</ul>
 
-			<!-- Right Side Of Navbar -->
+			Right Side Of Navbar
 			<ul class="navbar-nav ms-auto">
-				<!-- Authentication Links -->
+				Authentication Links
 				@guest
 					@if (Route::has('login'))
 						<li class="nav-item">
@@ -55,5 +74,5 @@
 				@endguest
 			</ul>
 		</div>
-	</div>
+	</div>-->
 </nav>
