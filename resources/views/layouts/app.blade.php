1fe86378a30052876a2bf63bdabcfaf0c9626898
Nebula6 <eaglenebula286@gmail.com>
2022-02-03 08:13:39 +0000

    more scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/app.css">
+		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 		<title>{{ config('app.name', 'Team Project')  }}</title>
 	</head>
 	<body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css', [
-        require('tailwindcss'),
-    ]);
+    .sass('resources/scss/app.scss', 'public/css')
+    .version(),
+    );
