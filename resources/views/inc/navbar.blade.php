74f5960e3fc95384b3baf3bfa77c86d645956628
Nebula6 <eaglenebula286@gmail.com>
2022-02-03 11:52:10 +0000

    not gonna touch scss anymore, editied navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,6 +3,12 @@
 		<a class="navbar-brand" href="{{ url('/') }}">
 			{{ config('app.name', 'Laravel') }}
 		</a>
+		<a class="navbar-brand" href="{{ url('/about') }}">
+			About
+		</a>
+		<a class="navbar-brand" href="{{ url('/projects') }}">
+			Projects
+		</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
 			<span class="navbar-toggler-icon"></span>
 		</button>
