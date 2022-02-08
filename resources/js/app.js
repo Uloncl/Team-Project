4609dcd1386f209d0287fe86f18b2ce24faff770
Nebula6 <eaglenebula286@gmail.com>
2022-02-08 09:42:25 +0000

    removing maps stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,1 @@
 require('./bootstrap');
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
--- resources/views/main/project.blade.php
+++ resources/views/main/project.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
 	<main class="container">
-		<div id="map">should be a map here</div>
+		<h1>project</h1>
 
-		<script
-		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBp4sNgTonXACL6BKvEjM1-RFliJNFC_PI&callback=initMap&v=weekly"
-		async
-		></script>
 	</main>
 @endsection
