c24b59656f42698fc227aac01f7163a6ff22184f
Nebula6 <eaglenebula286@gmail.com>
2022-02-18 17:57:20 +0000

    small changes

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,19 +1,17 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<script type='text/javascript' src='https://www.bing.com/api/maps/mapcontrol?key=AnjJYWrg1k5GYbX6LTEzzoa6fJnBE0fWTLdDGfqvBcI6m5-9512OFRlW7FdkHuvF'></script>
+		<title>{{ config('app.name', 'Laravel') }}</title>
+		<script src="{{ asset('js/app.js') }}" ></script>
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-	<title>{{ $view_name }}</title>
-	<script src="{{ asset('js/app.js') }}" ></script>
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-</head>
+	</head>
 	<body>
 		<div id="app">
 			<x-navbar/>
