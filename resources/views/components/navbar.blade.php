fa3a87d760405327cbe0c5cc848392b92232b251
Nebula6 <eaglenebula286@gmail.com>
2022-02-25 21:55:05 +0000

    attempted fix of sass and scss, run npm run dev, navbar now looks better

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,9 +2,9 @@
 	<div class="container">
 		<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 			<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-				<li><a href="{{ url('/') }}" class="nav-link px-2 link-dark">{{ config('app.name', 'Laravel') }}</a></li>
-				<li><a href="{{ url('/about') }}" class="nav-link px-2 link-dark">About</a></li>
-				<li><a href="{{ url('/project') }}" class="nav-link px-2 link-dark">Map</a></li>
+				<li><a href="{{ url('/') }}" class="nav-link px-2 text-black">{{ config('app.name', 'Laravel') }}</a></li>
+				<li><a href="{{ url('/about') }}" class="nav-link px-2 text-black">About</a></li>
+				<li><a href="{{ url('/project') }}" class="nav-link px-2 text-black">Map</a></li>
 			</ul>
 
 			<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
@@ -28,8 +28,8 @@
 			@else
 
 				<div class="col-md-3">
-					<a class="btn btn-outline-primary me-2" href="{{ url('/login') }}" role="button">Login</button>
-					<a class="btn btn-primary" href="{{ url('/register') }}" role="button">Register</a>
+					<a class="btn btn-outline-secondary me-2" href="{{ url('/login') }}" role="button">Login</button>
+					<a class="btn btn-secondary" href="{{ url('/register') }}" role="button">Register</a>
 				</div>
 
 			@endauth
