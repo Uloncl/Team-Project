712c4bc71d12abd5c34b90b9ba8496a244be8d42
Andrea.L <a.lafaucideleo7652@student.leedsbeckett.ac.uk>
2022-03-05 16:15:23 +0000

    Changed Pricing page bg colour

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1,6 +1,6 @@
 <x-app>
-    <main class="container">
-        <h1 style="background-color: powderblue">This is pricing</h1>
-        <p style="background-color: tomato">Beep boop.</p>
+    <main class="container text-light bg-dark">
+        <h1>This is pricing</h1>
+        <p> Beep boop.</p>
     </main>
 </x-app>
