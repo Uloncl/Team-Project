d8898d6bf49ab3436bba15fd9fd706d8d1cb0a41
Nebula6 <eaglenebula286@gmail.com>
2022-03-05 17:17:57 +0000

    light mods and better navbar, backend broken

--- resources/views/product.blade.php
+++
@@ -1,6 +0,0 @@
-<x-app>
-    <main class="container">
-        <h1 style="background-color: powderblue">This is product</h1>
-        <p style="background-color: tomato">Beep boop.</p>
-    </main>
-</x-app>
