d48a14dde7717ff7c09d77c8b26d1649a5f51d3c
Andrea.L <a.lafaucideleo7652@student.leedsbeckett.ac.uk>
2022-03-05 16:06:53 +0000

    Master modified

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -41,17 +41,11 @@
 -->
 <header class="bg-dark site-header sticky-top py-1 text-light">
   <nav class="container d-flex flex-column flex-md-row justify-content-between">
-<<<<<<< HEAD
-    <a class="py-2" href="#" aria-label="Product">
-      	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24">
-			<title>Product</title>
-			<circle cx="12" cy="12" r="10"></circle>
 			<path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path>
 			</svg>
-=======
     <a class="py-2" href="{{ url('/') }}" aria-label="Product">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"></circle><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path></svg>
->>>>>>> master
+      
     </a>
     <a class="py-2 d-none d-md-inline-block nav-link text-light" href="tour">Tour</a>
     <a class="py-2 d-none d-md-inline-block nav-link text-light" href="product">Product</a>
