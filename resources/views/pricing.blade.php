0b0270fa66efac98c6088683896be8ba9cb63607
aryanprince <A.Santy6483@student.leedsbeckett.ac.uk>
2022-03-05 16:00:05 +0000

    Added navbar functionality

---
+++ resources/views/pricing.blade.php
@@ -0,0 +1,6 @@
+<x-app>
+    <main class="container">
+        <h1 style="background-color: powderblue">This is pricing</h1>
+        <p style="background-color: tomato">Beep boop.</p>
+    </main>
+</x-app>
