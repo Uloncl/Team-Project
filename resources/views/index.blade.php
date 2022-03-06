9a078858f77d68e0c4bc732e7a33a19e72559679
Andrea.L <a.lafaucideleo7652@student.leedsbeckett.ac.uk>
2022-03-06 18:18:24 +0000

    Nav Bar components changed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,10 +42,12 @@
             </div>
           </div>
         </div>
+        
         <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
+        
         <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
