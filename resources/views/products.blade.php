c93790c37770d82b8bdb2e41771d290b3f94ae8a
aryanprince <A.Santy6483@student.leedsbeckett.ac.uk>
2022-03-09 16:16:57 +0000

    Updated games listing

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,41 +3,49 @@
 		<h1>{{ $product }}</h1>
 		
 		<div class="container">
-			<div class="row xl-8 md-6 sm-3">
+			<div class="row">
 				
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/1632952d4c85e981bf4ef0bbca426cb4.png" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">Elden Ring</h5>
-					  <p class="card-text">FromSoftware</p>
-					  <a href="#" class="btn btn-primary">£49.99</a>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/13e02498b2ae3df0c9078db2f9eb9b2d.webp" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">Elden Ring</h5>
+						  <p class="card-text">FromSoftware</p>
+						  <a href="https://store.steampowered.com/app/1245620/ELDEN_RING/" class="btn btn-primary">£49.99 on Steam</a>
+						</div>
 					</div>
 				</div>
 
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/f8dcde33b4f39275447ccf67f8e90ef2.jpg" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">Dying Light 2</h5>
-					  <p class="card-text">TechLand</p>
-					  <a href="#" class="btn btn-primary">£54.99</a>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/561adbb4e6094bef3c29e38ceb6bd929.png" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">Cyberpunk 2077</h5>
+						  <p class="card-text">CD Projekt Red</p>
+						  <a href="https://www.gog.com/en/game/cyberpunk_2077" class="btn btn-primary">£49.90 on GOG</a>
+						</div>
 					</div>
 				</div>
 
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/914179592aa3523210dd00c0bb030c30.png" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">Assassin's Creed Valhalla</h5>
-					  <p class="card-text">Ubisoft Montreal</p>
-					  <a href="#" class="btn btn-primary">£19.99</a>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/914179592aa3523210dd00c0bb030c30.png" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">Assassin's Creed Valhalla</h5>
+						  <p class="card-text">Ubisoft Montreal</p>
+						  <a href="https://store.ubi.com/uk/game?pid=5e849c6c5cdf9a21c0b4e731&dwvar_5e849c6c5cdf9a21c0b4e731_Platform=pcdl&edition=Standard%20Edition&source=detail" class="btn btn-primary">£49.99 on Ubisoft</a>
+						</div>
 					</div>
 				</div>
-
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/5855660034a74cfe0e5fc8d57d17f4ac.png" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">God of War</h5>
-					  <p class="card-text">Santa Monica Studio</p>
-					  <a href="#" class="btn btn-primary">£39.99</a>
+				
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/5855660034a74cfe0e5fc8d57d17f4ac.png" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">God of War</h5>
+						  <p class="card-text">Santa Monica Studio</p>
+						  <a href="https://www.epicgames.com/store/en-US/p/dying-light-2-stay-human" class="btn btn-primary">£39.99 on Epic</a>
+						</div>
 					</div>
 				</div>
 				
@@ -45,39 +53,47 @@
 
 			<div class="row">
 				
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/43f6943d7a020cdcb8d27aebb9d96221.png" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">GTA V</h5>
-					  <p class="card-text">Rockstar Games</p>
-					  <a href="#" class="btn btn-primary">£14.99</a>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/43f6943d7a020cdcb8d27aebb9d96221.png" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">GTA V</h5>
+						  <p class="card-text">Rockstar Games</p>
+						  <a href="#" class="btn btn-primary">£14.99</a>
+						</div>
 					</div>
 				</div>
 
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/0ea50e88822a30ab6cd93ba4a082d614.png" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">Batman Arkham Knight</h5>
-					  <p class="card-text">Rocksteady Studios</p>
-					  <a href="#" class="btn btn-primary">£15.99</a>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/0ea50e88822a30ab6cd93ba4a082d614.png" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">Batman Arkham Knight</h5>
+						  <p class="card-text">Rocksteady Studios</p>
+						  <a href="#" class="btn btn-primary">£15.99</a>
+						</div>
 					</div>
 				</div>
 
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/3d02d7d499c5801377ccbddcf8b0759e.png" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">Riders Republic</h5>
-					  <p class="card-text">Ubisoft Annecy</p>
-					  <a href="#" class="btn btn-primary">£24.99</a>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/3d02d7d499c5801377ccbddcf8b0759e.png" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">Riders Republic</h5>
+						  <p class="card-text">Ubisoft Annecy</p>
+						  <a href="#" class="btn btn-primary">£24.99</a>
+						</div>
 					</div>
 				</div>
 
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/458e998ed46376eb777b4df39beb02ce.png" alt="Card image cap">
-					<div class="card-body">
-					  <h5 class="card-title">Ghostrunner</h5>
-					  <p class="card-text">505 Games</p>
-					  <a href="#" class="btn btn-primary">£9.99</a>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/458e998ed46376eb777b4df39beb02ce.png" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">Ghostrunner</h5>
+						  <p class="card-text">505 Games</p>
+						  <a href="#" class="btn btn-primary">£9.99</a>
+						</div>
 					</div>
 				</div>
 				
