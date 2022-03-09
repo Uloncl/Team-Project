e3551ca20e3f131b7df784d15b153ab72cba5a5c
Andrea.L <a.lafaucideleo7652@student.leedsbeckett.ac.uk>
2022-03-09 18:18:08 +0000

    Heart added on card

--- resources/views/components/game-vert-card.blade.php
+++ resources/views/components/game-vert-card.blade.php
@@ -5,6 +5,7 @@
             <h5 class="card-title">{{ $product['name'] }}</h5>
             <p class="card-text">{{ $product['dev'] }}</p>
             <a href="{{ $product['link'] }}" class="btn btn-primary">£{{ $product['best'] }} on Steam</a>
+            <a href="#" class="btn btn-danger align-right" style="float: right;"><i class="bi bi-heart"></i></a>
         </div>
     </div>
 </div>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,7 +3,6 @@
 		<div class="container">
 			<div class="row p-4 pb-0 m-4">
 				
-<<<<<<< HEAD
 				<h1>{{ $title }}</h1>
 
 				@forelse ($products as $product)
@@ -17,45 +16,6 @@
 
 
 				<!--<div class="col">
-=======
-				<div class="col">
-					<div class="card" style="width: 15rem;">
-						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/13e02498b2ae3df0c9078db2f9eb9b2d.webp" alt="Card image cap">
-						<div class="card-body">
-						  <h5 class="card-title">Elden Ring</h5>
-						  <p class="card-text">FromSoftware</p>
-						  <a href="https://store.steampowered.com/app/1245620/ELDEN_RING/" class="btn btn-primary">£49.99 on Steam</a>
-						  <a href="#" class="btn btn-danger align-right" style="float: right;"><i class="bi bi-heart"></i></a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col">
-					<div class="card" style="width: 15rem;">
-						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/561adbb4e6094bef3c29e38ceb6bd929.png" alt="Card image cap">
-						<div class="card-body">
-						  <h5 class="card-title">Cyberpunk 2077</h5>
-						  <p class="card-text">CD Projekt Red</p>
-						  <a href="https://www.gog.com/en/game/cyberpunk_2077" class="btn btn-primary">£49.90 on GOG</a>
-						  <a href="#" class="btn btn-danger align-right" style="float: right;"><i class="bi bi-heart"></i></a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col">
-					<div class="card" style="width: 15rem;">
-						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/914179592aa3523210dd00c0bb030c30.png" alt="Card image cap">
-						<div class="card-body">
-						  <h5 class="card-title">AC Valhalla</h5>
-						  <p class="card-text">Ubisoft Montreal</p>
-						  <a href="https://store.ubi.com/uk/game?pid=5e849c6c5cdf9a21c0b4e731&dwvar_5e849c6c5cdf9a21c0b4e731_Platform=pcdl&edition=Standard%20Edition&source=detail" class="btn btn-primary">£49.99 on Ubisoft</a>
-						  <a href="#" class="btn btn-danger align-right" style="float: right;"><i class="bi bi-heart"></i></a>
-						</div>
-					</div>
-				</div>
-				
-				<div class="col">
->>>>>>> 8d43fe6263883e7e0a7a401f3d6c665e2dcde05a
 					<div class="card" style="width: 15rem;">
 						<img class="card-img-top" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/5855660034a74cfe0e5fc8d57d17f4ac.png" alt="Card image cap">
 						<div class="card-body">
@@ -118,13 +78,8 @@
 						  <a href="#" class="btn btn-danger align-right" style="float: right;"><i class="bi bi-heart"></i></a>
 						</div>
 					</div>
-<<<<<<< HEAD
 				</div>-->
 				
-=======
-				</div>
-
->>>>>>> 8d43fe6263883e7e0a7a401f3d6c665e2dcde05a
 			</div>
 		</div>
 	</main>
