041a0b169019c1f87fd7aa0c193944d647d50ada
Lewis M <l.mann3999@student.leedsbeckett.ac.uk>
2022-03-09 19:20:53 +0000

    spacing

--- resources/views/components/game-vert-card.blade.php
+++ resources/views/components/game-vert-card.blade.php
@@ -5,7 +5,7 @@
             <h5 class="card-title">{{ $product['name'] }}</h5>
             <p class="card-text">{{ $product['dev'] }}</p>
             <a href="{{ $product['link'] }}" class="btn btn-primary p-2">£{{ $product['best'] }}</a>
-            <a href="#" class="text-dark align-right float-end d-inline-block text-center p-2"><i class="bi bi-heart"></i></a>
+            <a href="#" class="text-dark align-right float-end d-inline-block text-center p-2 px-3"><i class="bi bi-heart"></i></a>
         </div>
     </div>
 </div>
