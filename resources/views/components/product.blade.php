a1d9a02fbe7b974b284ee4226f862010477366a6
aryanprince <A.Santy6483@student.leedsbeckett.ac.uk>
2022-03-09 15:08:46 +0000

    Product component has a slot now

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,7 +1,6 @@
 <div class="col-sm bg-dark m-3 text-center text-white overflow-hidden">
     <div class="my-3 py-3">
-        <h2 class="display-5 text-light">Another headline</h2>
-        <p class="lead text-light">And an even wittier subheading.</p>
+        {{ $slot }}
     </div>
     <!--<div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>-->
 </div>
