2d5d50cdda1062424181920acd40cdbe66d8c075
aryanprince <A.Santy6483@student.leedsbeckett.ac.uk>
2022-03-09 20:18:48 +0000

    Some more changes to posters and game titles

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -26,29 +26,29 @@ class PagesController extends Controller
 				$products = [
 					[//1
 						"name"  => "Elden Ring",
-						"dev"   => "From Software",
-						"pub"   => "From Software",
+						"dev"   => "FromSoftware",
+						"pub"   => "FromSoftware",
 						"link"  => "https://store.steampowered.com/app/1245620/ELDEN_RING/",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/13e02498b2ae3df0c9078db2f9eb9b2d.webp",
 						"best"  => "49.99"
 					],[//2
 						"name"  => "Cyberpunk 2077",
-						"dev"   => "CD Project Red",
-						"pub"   => "CD Project Red",
+						"dev"   => "CD PROJEKT RED",
+						"pub"   => "CD PROJEKT RED",
 						"link"  => "https://www.gog.com/en/game/cyberpunk_2077",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/2d0d6a7929c9542ee1d63b2065a40983.webp",
 						"best"  => "49.99"
 					],[//3
 						"name"  => "Tetris Effect",
-						"dev"   => "Sony Interactive",
-						"pub"   => "Sony Interactive",
+						"dev"   => "Enhance",
+						"pub"   => "Enhance",
 						"link"  => "https://store.steampowered.com/app/1003590/Tetris_Effect_Connected/",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/dd5cdc1c8ca1c1a34941e8d3cb5abc1c.webp",
 						"best"  => "49.99"
 					],[//4
 						"name"  => "God of War",
-						"dev"   => "Santa Monica Studio",
-						"pub"   => "Santa Monica Studio",
+						"dev"   => "PlayStation PC",
+						"pub"   => "PlayStation PC",
 						"link"  => "https://store.steampowered.com/app/1593500/God_of_War/",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/d1e6275ba67b9deba97c36664a972a9c.webp",
 						"best"  => "39.99"
@@ -61,15 +61,15 @@ class PagesController extends Controller
 						"best"  => "14.99"
 					],[//6
 						"name"  => "Arkham Knight",
-						"dev"   => "Rocksteady Games",
-						"pub"   => "Rocksteady",
+						"dev"   => "Warner Bros",
+						"pub"   => "Warner Bros",
 						"link"  => "https://store.steampowered.com/app/208650/Batman_Arkham_Knight/",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/a241000aa7e57b9fe80ebecfddd317af.png",
 						"best"  => "15.99"
 					],[//7
 						"name"  => "Sniper Elite v2",
-						"dev"   => "Rebellion Developments",
-						"pub"   => "Rebellion Developments",
+						"dev"   => "Rebellion",
+						"pub"   => "Rebellion",
 						"link"  => "https://store.steampowered.com/app/63380/Sniper_Elite_V2/",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/37c7e42a816d7df558110a5b877c3412.webp",
 						"best"  => "14.99"
@@ -82,15 +82,15 @@ class PagesController extends Controller
 						"best"  => "9.99"
 					],[//9
 						"name"  => "Monster Hunter",
-						"dev"   => "Capcom Co.",
-						"pub"   => "Capcom Co.",
+						"dev"   => "CAPCOM",
+						"pub"   => "CAPCOM",
 						"link"  => "https://store.steampowered.com/app/582010/Monster_Hunter_World/",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/601f9a238f7d2f9faeaa2a4e31e13e66.png",
 						"best"  => "24.99"
 					],[//10
 						"name"  => "DOOM Eternal",
-						"dev"   => "id Software",
-						"pub"   => "id Software",
+						"dev"   => "Bethesda Softworks",
+						"pub"   => "Bethesda Softworks",
 						"link"  => "https://store.steampowered.com/app/782330/DOOM_Eternal/",
 						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/7a37e5ef930630650790791fe509e8dc.png",
 						"best"  => "19.99"
