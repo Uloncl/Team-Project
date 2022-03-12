8695c29ccf6f4d03206f9ca04d529de6bf698f12
Lewis M <l.mann3999@student.leedsbeckett.ac.uk>
2022-03-12 09:42:46 +0000

    2 components for all categories cleaner

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -19,241 +19,305 @@ class PagesController extends Controller
 	public function settings() {
 		return view('settings');
 	}
-	public function products($category) {
+	public function products($category, $orientation = "horizontal") {
 		switch ($category) {
 			case "games":
-				$category = "Games";
 				$products = [
 					[//1
 						"name"  => "Elden Ring",
 						"dev"   => "FromSoftware",
 						"pub"   => "FromSoftware",
 						"link"  => "https://store.steampowered.com/app/1245620/ELDEN_RING/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/13e02498b2ae3df0c9078db2f9eb9b2d.webp",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/13e02498b2ae3df0c9078db2f9eb9b2d.webp",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/e8a2134c849b73bd279caa9f87c94c40.png",
 						"best"  => "49.99"
 					],[//2
 						"name"  => "Cyberpunk 2077",
 						"dev"   => "CD PROJEKT RED",
 						"pub"   => "CD PROJEKT RED",
 						"link"  => "https://www.gog.com/en/game/cyberpunk_2077",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/561adbb4e6094bef3c29e38ceb6bd929.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/561adbb4e6094bef3c29e38ceb6bd929.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/32c3d0cdaa575bc25350c7fce4ad91e2.jpg",
 						"best"  => "49.99"
 					],[//3
 						"name"  => "Tetris Effect",
 						"dev"   => "Enhance",
 						"pub"   => "Enhance",
 						"link"  => "https://store.steampowered.com/app/1003590/Tetris_Effect_Connected/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/ec1e28295c9fe4e013bbc4c45e2ed0ea.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/ec1e28295c9fe4e013bbc4c45e2ed0ea.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/ec1e28295c9fe4e013bbc4c45e2ed0ea.png",
 						"best"  => "49.99"
 					],[//4
 						"name"  => "God of War",
 						"dev"   => "PlayStation PC",
 						"pub"   => "PlayStation PC",
 						"link"  => "https://store.steampowered.com/app/1593500/God_of_War/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/5855660034a74cfe0e5fc8d57d17f4ac.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/5855660034a74cfe0e5fc8d57d17f4ac.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/5855660034a74cfe0e5fc8d57d17f4ac.png",
 						"best"  => "39.99"
 					],[//5
 						"name"  => "GTA V",
 						"dev"   => "Rockstar Games",
 						"pub"   => "Rockstar Games",
 						"link"  => "https://www.epicgames.com/store/en-US/p/grand-theft-auto-v",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/43f6943d7a020cdcb8d27aebb9d96221.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/43f6943d7a020cdcb8d27aebb9d96221.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/43f6943d7a020cdcb8d27aebb9d96221.png",
 						"best"  => "14.99"
 					],[//6
 						"name"  => "Arkham Knight",
 						"dev"   => "Warner Bros",
 						"pub"   => "Warner Bros",
 						"link"  => "https://store.steampowered.com/app/208650/Batman_Arkham_Knight/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/0ea50e88822a30ab6cd93ba4a082d614.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/0ea50e88822a30ab6cd93ba4a082d614.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/0ea50e88822a30ab6cd93ba4a082d614.png",
 						"best"  => "15.99"
 					],[//7
 						"name"  => "Sniper Elite v2",
 						"dev"   => "Rebellion",
 						"pub"   => "Rebellion",
 						"link"  => "https://store.steampowered.com/app/63380/Sniper_Elite_V2/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/c9766f9867f5bbe212da9b9c59726cb8.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/c9766f9867f5bbe212da9b9c59726cb8.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/c9766f9867f5bbe212da9b9c59726cb8.png",
 						"best"  => "14.99"
 					],[//8
 						"name"  => "Ghostrunner",
 						"dev"   => "505 Games",
 						"pub"   => "505 Games",
 						"link"  => "https://store.steampowered.com/app/1139900/Ghostrunner/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/2cf297f54a6678a3d0f225a64a2c6db3.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/2cf297f54a6678a3d0f225a64a2c6db3.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/2cf297f54a6678a3d0f225a64a2c6db3.png",
 						"best"  => "9.99"
 					],[//9
 						"name"  => "Monster Hunter",
 						"dev"   => "CAPCOM",
 						"pub"   => "CAPCOM",
 						"link"  => "https://store.steampowered.com/app/582010/Monster_Hunter_World/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/36f0052da9cf0f101267b6e7f6629248.jpg",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/36f0052da9cf0f101267b6e7f6629248.jpg",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/36f0052da9cf0f101267b6e7f6629248.jpg",
 						"best"  => "24.99"
 					],[//10
 						"name"  => "DOOM Eternal",
 						"dev"   => "Bethesda Softworks",
 						"pub"   => "Bethesda Softworks",
 						"link"  => "https://store.steampowered.com/app/782330/DOOM_Eternal/",
-						"img"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/7a37e5ef930630650790791fe509e8dc.png",
+						"img-hori"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/7a37e5ef930630650790791fe509e8dc.png",
+						"img-vert"   => "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/7a37e5ef930630650790791fe509e8dc.png",
 						"best"  => "19.99"
 					]
 				];
+				
+				
 				return view('products', [
-					"category"    => $category,
-					"products" => $products
+					"category"    	=> $category,
+					"orientation"   => $orientation,
+					"products" 		=> $products
 				]);
 				break;
 			case "components":
-				$category = "Components";
 				$products = [
 					[//1
 						"name"  => "NVIDIA GeForce RTX 3090 FE",
-						"desc"   => "VRAM: 24GB GDDR6X",
+						"type"  => "gpu",
+						"specs"   => [
+							"CUDA cores: 10496",
+							"boost clock: 1.7GHz",
+							"memory: 24GB"
+						],
 						"link"  => "#",
-						"img-hor"   => "..\img\components\hor-3090.png",
+						"img-hori"   => "..\img\components\hor-3090.png",
 						"img-vert"   => "..\img\components/vert-3090.png",
 						"best"  => "2100.00"
 					],
 					[//2
 						"name"  => "NZXT Kraken Z73 AIO Cooler",
-						"desc"   => "Fans: 360mm (3x120mm)",
+						"type"  => "water cooler",
+						"specs"   => [
+							"pump speed: 10496"
+						],
 						"link"  => "#",
-						"img-hor"   => "..\img\components\hor-cooler.png",
+						"img-hori"   => "..\img\components\hor-cooler.png",
 						"img-vert"   => "..\img\components/vert-cooler.png",
 						"best"  => "249.99"
 					],
 					[//3
 						"name"  => "Intel Core i9-11900K Desktop Processor",
-						"desc"   => "Cores: 8C+8c/24T",
+						"type"  => "cpu",
+						"specs"   => [
+							"cores: 8",
+							"threads: 16",
+							"max clock speed: 5.30Ghz"
+						],
 						"link"  => "#",
-						"img-hor"   => "..\img\components\hor-cpu.png",
+						"img-hori"   => "..\img\components\hor-cpu.png",
 						"img-vert"   => "..\img\components/vert-cpu.png",
 						"best"  => "545.00"
 					],
 					[//4
 						"name"  => "G.SKILL Trident Z RGB Series",
-						"desc"   => "Kits: 16 GB DDR4",
+						"type"  => "ram",
+						"specs"   => [
+							"capacity: 64GB",
+							"type: 288-pin DDR4 SDRAM",
+							"speed: 3600"
+						],
 						"link"  => "#",
-						"img-hor"   => "..\img\components\hor-ram.png",
+						"img-hori"   => "..\img\components\hor-ram.png",
 						"img-vert"   => "..\img\components/vert-ram.png",
 						"best"  => "112.99"
 					],
 					[//5
 						"name"  => "Samsung 980 1 TB Solid State Drive",
-						"desc"   => "Connectivity: NVMe M.2",
+						"type"  => "SSD",
+						"specs"   => [
+							"capacity: 1TB",
+							"read speed: 7GB/s",
+							"write speed: 5.1GB/s"
+						],
 						"link"  => "#",
-						"img-hor"   => "..\img\components\hor-ssd.png",
+						"img-hori"   => "..\img\components\hor-ssd.png",
 						"img-vert"   => "..\img\components/vert-ssd.png",
 						"best"  => "110.00"
 					]
 				];
 
+				
 				return view('products', [
-					"category"    => $category,
-					"products" => $products
+					"category"    	=> $category,
+					"orientation"   => $orientation,
+					"products" 		=> $products
 				]);
 				break;
 			case "consoles":
-				$category = "Consoles";
 				$products = [
 					[//1
 						"name"  => "PlayStation 5 Disc",
-						"dev"   => "Disc Edition",
-						"pub"   => "Disc Edition",
+						"brand"   => "Sony",
+						"desc"   => "Disk Edition",
 						"link"  => "https://www.playstation.com/en-gb/ps5/",
-						"img"   => "../img/consoles-ps5-disc.png",
+						"img-hori"   => "../img/consoles-ps5-disc.png",
+						"img-vert"   => "../img/consoles-ps5-disc.png",
 						"best"  => "449.99"
-					],
-					[//1
+					],[//1
 						"name"  => "PlayStation 5 Digital",
-						"dev"   => "Digital Edition",
-						"pub"   => "Digital Edition",
+						"brand"   => "Sony",
+						"desc"   => "Digital Edition",
 						"link"  => "https://www.playstation.com/en-gb/ps5/",
-						"img"   => "../img/consoles-ps5-digital.png",
+						"img-hori"   => "../img/consoles-ps5-digital.png",
+						"img-vert"   => "../img/consoles-ps5-digital.png",
 						"best"  => "359.99"
-					],
-					[//1
+					],[//1
 						"name"  => "Xbox Series X",
-						"dev"   => "Microsoft",
-						"pub"   => "Microsoft",
+						"brand"   => "Microsoft",
+						"desc"   => "Best Version",
 						"link"  => "https://www.xbox.com/en-GB/consoles/xbox-series-x",
-						"img"   => "../img/consoles-xbox-series-x.png",
+						"img-hori"   => "../img/consoles-xbox-series-x.png",
+						"img-vert"   => "../img/consoles-xbox-series-x.png",
 						"best"  => "449.99"
-					],
-					[//1
+					],[//1
 						"name"  => "Xbox Series S",
-						"dev"   => "Microsoft",
-						"pub"   => "Microsoft",
+						"brand"   => "Microsoft",
+						"desc"   => "Budget Version",
 						"link"  => "https://www.xbox.com/en-GB/consoles/xbox-series-s",
-						"img"   => "../img/consoles-xbox-series-s.png",
+						"img-hori"   => "../img/consoles-xbox-series-s.png",
+						"img-vert"   => "../img/consoles-xbox-series-s.png",
 						"best"  => "249.99"
-					],
-					[//1
+					],[//1
 						"name"  => "Nintendo Switch",
-						"dev"   => "OLED Edition",
-						"pub"   => "OLED Edition",
+						"brand"   => "Nintendo",
+						"desc"   => "OLED Edition",
 						"link"  => "https://www.nintendo.com/switch/oled-model/",
-						"img"   => "../img/consoles-switch-oled.png",
+						"img-hori"   => "../img/consoles-switch-oled.png",
+						"img-vert"   => "../img/consoles-switch-oled.png",
 						"best"  => "309.99"
 					]
 				];
 
+				
 				return view('products', [
-					"category"    => $category,
-					"products" => $products
+					"category"    	=> $category,
+					"orientation"   => $orientation,
+					"products" 		=> $products
 				]);
 				break;
 			case "prebuilds":
-				$category = "Prebuilds";
 				$products = [
 					[//1
 						"name"  => "Corsair One i300",
-						"desc"   => "i9-12900K / RTX 3080 Ti / 64GB DDR5",
+						"specs"   => [
+							"cpu" => "i9-12900K", 
+							"gpu" => "RTX 3080 Ti", 
+							"ram" => "64GB DDR5"
+						],
 						"link"  => "#",
 						"img-vert"   => "..\img\prebuilds/vert-corsair.png",
-						"img-hor"   => "..\img\prebuilds/hor-corsair.png",
+						"img-hori"   => "..\img\prebuilds/hor-corsair.png",
 						"best"  => "1000"
 					],
 					[//2
 						"name"  => "Predator Orion 3000",
-						"desc"   => "i7-8700K / GTX 1080 / 64GB DDR4",
+						"specs"   => [
+							"cpu" => "i9-12900K", 
+							"gpu" => "RTX 3080 Ti", 
+							"ram" => "64GB DDR5"
+						],
 						"link"  => "#",
 						"img-vert"   => "..\img\prebuilds/vert-acer.png",
-						"img-hor"   => "..\img\prebuilds/hor-acer.png",
+						"img-hori"   => "..\img\prebuilds/hor-acer.png",
 						"best"  => "1000"
 					],
 					[//3
 						"name"  => "Asus ROG GA15",
-						"desc"   => "R7-3800X / RTX 2070S / 16GB DDR4",
+						"specs"   => [
+							"cpu" => "i9-12900K", 
+							"gpu" => "RTX 3080 Ti", 
+							"ram" => "64GB DDR5"
+						],
 						"link"  => "#",
 						"img-vert"   => "..\img\prebuilds/vert-asus.png",
-						"img-hor"   => "..\img\prebuilds/hor-asus.png",
+						"img-hori"   => "..\img\prebuilds/hor-asus.png",
 						"best"  => "1000"
 					],
 					[//4
 						"name"  => "Alienware R12",
-						"desc"   => "i9-10900K / RTX 3090 / 32GB DDR4",
+						"specs"   => [
+							"cpu" => "i9-12900K", 
+							"gpu" => "RTX 3080 Ti", 
+							"ram" => "64GB DDR5"
+						],
 						"link"  => "#",
 						"img-vert"   => "..\img\prebuilds/vert-alienware.png",
-						"img-hor"   => "..\img\prebuilds/hor-alienware.png",
+						"img-hori"   => "..\img\prebuilds/hor-alienware.png",
 						"best"  => "1000"
 					],
 					[//5
 						"name"  => "HP Omen 30L",
-						"desc"   => "i9-10850K / RTX 3090 / 16GB DDR4",
+						"specs"   => [
+							"cpu" => "i9-12900K", 
+							"gpu" => "RTX 3080 Ti", 
+							"ram" => "64GB DDR5"
+						],
 						"link"  => "#",
 						"img-vert"   => "..\img\prebuilds/vert-hp.png",
-						"img-hor"   => "..\img\prebuilds/hor-hp.png",
+						"img-hori"   => "..\img\prebuilds/hor-hp.png",
 						"best"  => "1000"
 					]
 				];
-
+				
+				
 				return view('products', [
-					"category"    => $category,
-					"products" => $products
+					"category"    	=> $category,
+					"orientation"   => $orientation,
+					"products" 		=> $products
 				]);
 				break;
 			default:
 				$category = "no dont do that";
-				return view('products', compact('category'));
+				
+				return view('products', [
+					"category"    	=> $category,
+					"orientation"   => $orientation,
+					"products" 		=> Null
+				]);
 				break;
 		}
 	}
---
+++ resources/views/components/card-vert-product.blade.php
@@ -0,0 +1,51 @@
+<div class="col my-3">
+    <div class="card">
+        <div class="row">
+            <div class="col-sm-3">
+                <img class="card-img" src="{{ $product['img-vert'] }}" alt="Image loading error">
+            </div>
+
+            <script>
+                function <?php echo ($product['name'] = str_replace(' ', '_', $product['name'])) ?>() {
+                    var heart = document.getElementById("<?php echo ($product['name'] = str_replace(' ', '_', $product['name'])) ?>");
+                    heart.classList.toggle('bi-heart');
+                    heart.classList.toggle('bi-heart-fill');
+                }
+            </script>
+
+            <div class="col-sm-8 row">
+                <div class="w-100 my-auto"><h5 class="col w-100 card-title mt-2">{{ $product['name'] }}</h5></div>
+
+                @if ($category == 'games')
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['dev'] }}</p></div>
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['pub'] }}</p></div>
+
+                @elseif ($category == 'components')
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['type'] }}</p></div>
+                    @foreach($product['specs'] as $spec)
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $spec }}</p></div>
+                    @endforeach
+
+                @elseif ($category == 'prebuilds')
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['specs']['cpu'] }}</p></div>
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['specs']['gpu'] }}</p></div>
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['specs']['ram'] }}</p></div>
+
+                @elseif ($category == 'consoles')
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['brand'] }}</p></div>
+                    <div class="w-100 my-auto"><p class="col w-100 card-text">{{ $product['desc'] }}</p></div>
+                @endif
+                
+                <div class="w-100 my-auto"><a href="{{ $product['link'] }}" class="col btn btn-primary mb-2">£{{ $product['best'] }}</a></div>
+            </div>
+            
+            <div class="col-sm-1 row">
+                <div class="w-100 my-auto">
+                    <a type="button" onclick="<?php echo ($product['name'] = str_replace(' ', '_', $product['name'])) ?>()" href="#" class="text-dark align-right float-end d-inline-block text-center p-2 px-3">
+                        <i id="<?php echo ($product['name'] = str_replace(' ', '_', $product['name'])) ?>" class="text-danger bi bi-heart"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
