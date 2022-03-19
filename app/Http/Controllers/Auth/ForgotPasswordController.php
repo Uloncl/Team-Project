ae0a1dfd2ab634dbbeec19a1a98fcee95ac29e37
Ben-Miles2601 <benjamin.p.miles2601@gmail.com>
2022-03-19 15:02:23 +0000

    Forgot Password Controller updated

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+
+ 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
-use Sentinel;
-use Reminder;
-use App\User;
-use Mail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ForgotPasswordController extends Controller
 {
@@ -22,33 +21,21 @@ class ForgotPasswordController extends Controller
     |
     */
 
-    use SendsPasswordResetEmails;
 
     
 	public function forgot() {
 		return view('auth.passwords.email');
 	}
-    
-	public function update(Request $request) {
-        $user = User::whereEmail($request->$email)->first(); 
-        if("Get email from database"){
-            return redirect()->back()->with(['error' => 'Email does not exist']);
-        }
 
-        $user = Sentinel::findById($user->id);
-        $reminder = Reminder::exists($user) ? : Reminder::create($user);
-        $this->sendEmail($user, $reminder->code);
-		return view('auth.passwords.email')->with('success', 'check you emails to verify that your password can be changed');
-	}
 
-    public function sendEmail($user, $code){
-        Mail:send(
-            'email.forgot',
-            ['user' => $user, 'code', $code],
-            function($message) use ($user){
-                $message->to($user->email);
-                $message->subject("Hello $user->name", "Reset your password."); 
-            }
-        );
+    public function update(Request $request){
+        
+        Request()->validate(['email'=>'required|email']);
+        
+        Mail::raw('It works', function($message){
+            $message->to(Request('email'))
+                ->subject("Hello");
+        });
+
     }
 }
