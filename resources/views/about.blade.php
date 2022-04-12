7a774bd2dfbc25684e05998a8655bfced203a9a2
Lewis M <l.mann3999@student.leedsbeckett.ac.uk>
2022-04-12 18:43:10 +0100

    fixed about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,79 +1,116 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
 <x-app>
+	<div class="d-flex" style="background-image:url('/img/teambg.png'); margin: 0; padding: 0">
+		<div class="justify-content-center align-items-center w-100">
+			<div class="text-center">
+				<h1 style="margin: 5%">MEET THE TEAM</h1>
+			</div>
 
-<!------ Include the above in your HEAD tag ---------->
+			<div class="row" style="margin: 10%">
+				<div class="card col m-2 p-0" style="width: 0; height: 15%">
+					<img src="/img/team/andrea.jpg" class="card-img-top" alt="Andrea" />
+					<div class="card-body">
+						<h5 class="card-title">Andrea La Fauci De Leo</h5>
+						<p class="card-text">Front End</p>
+						<div class="row mx-5 mb-2">
+							<div class="col text-center">
+								<a href="https://www.facebook.com/andrea.lafauci/">
+									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
+								</a>
+							</div>
+							<div class="col text-center">
+								<a href="https://www.linkedin.com/in/andrea-lafaucideleo/">
+									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 
-<section class="our-webcoderskull padding-lg">
-  <div class="container">
-    <div class="row heading heading-icon">
-        <h2>Meet the Team</h2>
-    </div>
-    <ul class="row list-inline d-flex justify-content-center">
-      <li class="col-12 col-md-6 col-lg-3">
-          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
-            <figure><img src="/img/team/andrea.jpg" class="img-responsive" alt=""></figure>
-            <h3><a href="#">Andrea La Fauci De Leo</a></h3>
-            <p>Front End and so on</p>
-            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
-              <li><a href="https://www.facebook.com/andrea.lafauci/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="https://www.linkedin.com/in/andrea-lafaucideleo/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-          </div>
-      </li>
+				<div class="card col m-2 p-0" style="width: 15%; height: 15%">
+					<img src="/img/team/lewis.jpg" class="card-img-top" alt="Andrea" />
+					<div class="card-body">
+						<h5 class="card-title">Lewis Mann</h5>
+						<p class="card-text">Back End</p>
+						<div class="row mx-5 mb-2">
+							<div class="col text-center">
+								<a href="https://www.facebook.com/andrea.lafauci/">
+									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
+								</a>
+							</div>
+							<div class="col text-center">
+								<a href="https://www.linkedin.com/in/andrea-lafaucideleo/">
+									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 
-      <li class="col-12 col-md-6 col-lg-3">
-          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
-            <figure><img src="/img/team/lewis.jpg" class="img-responsive" alt=""></figure>
-            <h3><a href="#">Lewis Mann </a></h3>
-            <p>Back End and so on</p>
-            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-          </div>
-      </li>
+				<div class="card col m-2 p-0" style="width: 15%; height: 15%">
+					<img src="/img/team/lewis.jpg" class="card-img-top" alt="Andrea" />
+					<div class="card-body">
+						<h5 class="card-title">Aryan Prince</h5>
+						<p class="card-text">Back End</p>
+						<div class="row mx-5 mb-2">
+							<div class="col text-center">
+								<a href="https://www.facebook.com/andrea.lafauci/">
+									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
+								</a>
+							</div>
+							<div class="col text-center">
+								<a href="https://www.linkedin.com/in/andrea-lafaucideleo/">
+									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 
-      <li class="col-12 col-md-6 col-lg-3">
-          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
-            <figure><img src="#" class="img-responsive" alt=""></figure>
-            <h3><a href="#">Aryan Prince</a></h3>
-            <p>Front End and so on</p>
-            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-          </div>
-       </li>
+				<div class="card col m-2 p-0" style="width: 15%; height: 15%">
+					<img src="/img/team/lewis.jpg" class="card-img-top" alt="Andrea" />
+					<div class="card-body">
+						<h5 class="card-title">Benjamin Miles</h5>
+						<p class="card-text">Back End</p>
+						<div class="row mx-5 mb-2">
+							<div class="col text-center">
+								<a href="https://www.facebook.com/andrea.lafauci/">
+									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
+								</a>
+							</div>
+							<div class="col text-center">
+								<a href="https://www.linkedin.com/in/andrea-lafaucideleo/">
+									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 
-      <li class="col-12 col-md-6 col-lg-3">
-          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
-            <figure><img src="#" class="img-responsive" alt=""></figure>
-            <h3><a href="#">Benjamin Miles </a></h3>
-            <p>Back end and so on</p>
-            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-          </div>
-      </li>
-
-	  <li class="col-12 col-md-6 col-lg-3">
-          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
-            <figure><img src="#" class="img-responsive" alt=""></figure>
-            <h3><a href="#">Mabel McDonald</a></h3>
-            <p>Front End and so on</p>
-            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-          </div>
-       </li>
-    </ul>
-  </div>
-</section>
-
+				<div class="card col m-2 p-0" style="width: 15%; height: 15%">
+					<img src="/img/team/lewis.jpg" class="card-img-top" alt="Andrea" />
+					<div class="card-body">
+						<h5 class="card-title">Mabel McDonald</h5>
+						<p class="card-text">Back End</p>
+						<div class="row mx-5 mb-2">
+							<div class="col text-center">
+								<a href="https://www.facebook.com/andrea.lafauci/">
+									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
+								</a>
+							</div>
+							<div class="col text-center">
+								<a href="https://www.linkedin.com/in/andrea-lafaucideleo/">
+									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </x-app>
