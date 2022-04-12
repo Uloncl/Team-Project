413820fae174389112225347e0124b038532fd31
Andrea.L <a.lafaucideleo7652@student.leedsbeckett.ac.uk>
2022-04-12 16:55:58 +0100

    Added about page and custom CSS for it.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,79 @@
-<x-app>
-	<div class="container">
-		<x-hor-card-game/>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-	</div>
+<x-app>
+
+<!------ Include the above in your HEAD tag ---------->
+
+<section class="our-webcoderskull padding-lg">
+  <div class="container">
+    <div class="row heading heading-icon">
+        <h2>Meet the Team</h2>
+    </div>
+    <ul class="row list-inline d-flex justify-content-center">
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
+            <figure><img src="/img/team/andrea.jpg" class="img-responsive" alt=""></figure>
+            <h3><a href="#">Andrea La Fauci De Leo</a></h3>
+            <p>Front End and so on</p>
+            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
+              <li><a href="https://www.facebook.com/andrea.lafauci/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/in/andrea-lafaucideleo/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
+            <figure><img src="/img/team/lewis.jpg" class="img-responsive" alt=""></figure>
+            <h3><a href="#">Lewis Mann </a></h3>
+            <p>Back End and so on</p>
+            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
+              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
+            <figure><img src="#" class="img-responsive" alt=""></figure>
+            <h3><a href="#">Aryan Prince</a></h3>
+            <p>Front End and so on</p>
+            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
+              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+       </li>
+
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
+            <figure><img src="#" class="img-responsive" alt=""></figure>
+            <h3><a href="#">Benjamin Miles </a></h3>
+            <p>Back end and so on</p>
+            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
+              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+
+	  <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px; border-radius: 15px;">
+            <figure><img src="#" class="img-responsive" alt=""></figure>
+            <h3><a href="#">Mabel McDonald</a></h3>
+            <p>Front End and so on</p>
+            <ul class="follow-us clearfix list-inline d-flex justify-content-center">
+              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+       </li>
+    </ul>
+  </div>
+</section>
+
 </x-app>
