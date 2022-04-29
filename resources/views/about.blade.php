4a026f09f2084f9ab336cc0a97c981067fbb0eb6
aryanprince <aryanprincesanty@icloud.com>
2022-04-29 14:54:21 +0100

    Added a basic description in about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,165 +1,257 @@
 <x-app>
-	<div class="d-flex" style="
+    <div
+        class="d-flex"
+        style="
             background-image: url('/img/teambg.png');
             margin: 0;
             padding: 0;
             background-size: cover;
-        ">
-		<div class="justify-content-center align-items-center w-100">
-			<div class="text-center">
-				<h1 style="margin: 5%">MEET THE TEAM</h1>
-			</div>
+        "
+    >
+        <div class="justify-content-center align-items-center w-100">
+            <div class="text-center">
+                <h1 style="margin: 5%">MEET THE TEAM</h1>
+            </div>
 
-			<div class="row" style="margin: 10%">
-				<div class="card col m-2 p-0" style="width: 0; height: 15%">
-					<img src="/img/team/andrea.jpg" class="card-img-top rounded-circle m-5" style="width: 65%; height: 65%" alt="Andrea" />
-					<div class="card-body">
-						<h5 class="card-title">Andrea La Fauci De Leo</h5>
-						<p class="card-text">
-							Scrum Master <br />
-							Front End Developer
-						</p>
-						<div class="row mx-6 mb-3">
-							<div class="col text-center">
-								<a href="https://www.facebook.com/andrea.lafauci/">
-									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
-								</a>
-							</div>
-							<div class="col text-center">
-								<a href="https://github.com/Bosurgi">
-									<i style="font-size: 2rem" class="m-1 bi bi-github"></i>
-								</a>
-							</div>
-							<div class="col text-center">
-								<a href="https://www.linkedin.com/in/andrea-lafaucideleo/">
-									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
+            <div class="row" style="margin: 10%">
+                <div class="card col m-2 p-0" style="width: 0; height: 15%">
+                    <img
+                        src="/img/team/andrea.jpg"
+                        class="card-img-top rounded-circle m-5"
+                        style="width: 65%; height: 65%"
+                        alt="Andrea"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">Andrea La Fauci De Leo</h5>
+                        <p class="card-text">
+                            Scrum Master <br />
+                            Front End Developer
+                        </p>
+                        <div class="row mx-6 mb-3">
+                            <div class="col text-center">
+                                <a
+                                    href="https://www.facebook.com/andrea.lafauci/"
+                                >
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-facebook"
+                                    ></i>
+                                </a>
+                            </div>
+                            <div class="col text-center">
+                                <a href="https://github.com/Bosurgi">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-github"
+                                    ></i>
+                                </a>
+                            </div>
+                            <div class="col text-center">
+                                <a
+                                    href="https://www.linkedin.com/in/andrea-lafaucideleo/"
+                                >
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-linkedin"
+                                    ></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-				<div class="card col m-2 p-0" style="width: 15%; height: 15%">
-					<img src="/img/team/lewis.jpg" class="card-img-top rounded-circle m-5" style="width: 65%; height: 65%" alt="Andrea" />
-					<div class="card-body">
-						<h5 class="card-title">Lewis Mann</h5>
-						<p class="card-text">
-							Lead Developer <br />
-							Back End Developer
-						</p>
-						<div class="row mx-6 mb-3">
+                <div class="card col m-2 p-0" style="width: 15%; height: 15%">
+                    <img
+                        src="/img/team/lewis.jpg"
+                        class="card-img-top rounded-circle m-5"
+                        style="width: 65%; height: 65%"
+                        alt="Andrea"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">Lewis Mann</h5>
+                        <p class="card-text">
+                            Lead Developer <br />
+                            Back End Developer
+                        </p>
+                        <div class="row mx-6 mb-3">
+                            <div class="col text-center">
+                                <a
+                                    href="https://www.facebook.com/andrea.lafauci/"
+                                >
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-facebook"
+                                    ></i>
+                                </a>
+                            </div>
 
-							<div class="col text-center">
-								<a href="https://www.facebook.com/andrea.lafauci/">
-									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
-								</a>
-							</div>
+                            <div class="col text-center">
+                                <a href="https://github.com/Nebula6">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-github"
+                                    ></i>
+                                </a>
+                            </div>
+                            <div class="col text-center">
+                                <a
+                                    href="https://www.linkedin.com/in/lewis-mann-0/"
+                                >
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-linkedin"
+                                    ></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-							<div class="col text-center">
-								<a href="https://github.com/Nebula6">
-									<i style="font-size: 2rem" class="m-1 bi bi-github"></i>
-								</a>
-							</div>
-							<div class="col text-center">
-								<a href="https://www.linkedin.com/in/lewis-mann-0/">
-									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
+                <div class="card col m-2 p-0" style="width: 15%; height: 15%">
+                    <img
+                        src="/img/team/aryan.jpg"
+                        class="card-img-top rounded-circle m-5"
+                        style="width: 65%; height: 65%"
+                        alt="Andrea"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">Aryan Prince</h5>
+                        <p class="card-text">
+                            UI/UX Designer <br />
+                            Front End Developer
+                        </p>
+                        <div class="row mx-6 mb-3">
+                            <div class="col text-center">
+                                <a
+                                    href="https://www.facebook.com/andrea.lafauci/"
+                                >
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-facebook"
+                                    ></i>
+                                </a>
+                            </div>
 
-				<div class="card col m-2 p-0" style="width: 15%; height: 15%">
-					<img src="/img/team/aryan.jpg" class="card-img-top rounded-circle m-5" style="width: 65%; height: 65%" alt="Andrea" />
-					<div class="card-body">
-						<h5 class="card-title">Aryan Prince</h5>
-						<p class="card-text">
-							UI/UX Designer <br />
-							Front End Developer
-						</p>
-						<div class="row mx-6 mb-3">
+                            <div class="col text-center">
+                                <a href="https://github.com/aryanprince">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-github"
+                                    ></i>
+                                </a>
+                            </div>
+                            <div class="col text-center">
+                                <a
+                                    href="https://www.linkedin.com/in/aryanprince/"
+                                >
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-linkedin"
+                                    ></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-							<div class="col text-center">
-								<a href="https://www.facebook.com/andrea.lafauci/">
-									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
-								</a>
-							</div>
+                <div
+                    class="card col m-2 p-0"
+                    style="width: 15%; height: 15%; background-size: cover"
+                >
+                    <img
+                        src="/img/team/ben.jpg"
+                        class="card-img-top rounded-circle m-5"
+                        style="width: 65%; height: 65%"
+                        alt="Andrea"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">Benjamin Miles</h5>
+                        <p class="card-text">
+                            Product Manager <br />
+                            Back End Developer
+                        </p>
+                        <div class="row mx-6 mb-3">
+                            <div class="col text-center">
+                                <a href="#">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-facebook"
+                                    ></i>
+                                </a>
+                            </div>
 
-							<div class="col text-center">
-								<a href="https://github.com/aryanprince">
-									<i style="font-size: 2rem" class="m-1 bi bi-github"></i>
-								</a>
-							</div>
-							<div class="col text-center">
-								<a href="https://www.linkedin.com/in/aryanprince/">
-									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
+                            <div class="col text-center">
+                                <a href="#">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-github"
+                                    ></i>
+                                </a>
+                            </div>
 
-				<div class="card col m-2 p-0" style="width: 15%; height: 15%; background-size: cover">
-					<img src="/img/team/ben.jpg" class="card-img-top rounded-circle m-5" style="width: 65%; height: 65%" alt="Andrea" />
-					<div class="card-body">
-						<h5 class="card-title">Benjamin Miles</h5>
-						<p class="card-text">
-							Product Manager <br />
-							Back End Developer
-						</p>
-						<div class="row mx-6 mb-3">
-							<div class="col text-center">
-								<a href="#">
-									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
-								</a>
-							</div>
+                            <div class="col text-center">
+                                <a href="#">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-linkedin"
+                                    ></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-							<div class="col text-center">
-								<a href="#">
-									<i style="font-size: 2rem" class="m-1 bi bi-github"></i>
-								</a>
-							</div>
+                <div class="card col m-2 p-0" style="width: 15%; height: 15%">
+                    <img
+                        src="/img/team/mabel.jpg"
+                        class="card-img-top rounded-circle m-5"
+                        style="width: 65%; height: 65%"
+                        alt="Andrea"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">Mabel McDonald</h5>
+                        <p class="card-text">
+                            Web Designer <br />
+                            Front End Developer
+                        </p>
+                        <div class="row mx-6 mb-3">
+                            <div class="col text-center">
+                                <a href="#">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-facebook"
+                                    ></i>
+                                </a>
+                            </div>
 
+                            <div class="col text-center">
+                                <a href="#">
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-github"
+                                    ></i>
+                                </a>
+                            </div>
 
-							<div class="col text-center">
-								<a href="#">
-									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="card col m-2 p-0" style="width: 15%; height: 15%">
-					<img src="/img/team/mabel.jpg" class="card-img-top rounded-circle m-5" style="width: 65%; height: 65%" alt="Andrea" />
-					<div class="card-body">
-						<h5 class="card-title">Mabel McDonald</h5>
-						<p class="card-text">
-							Web Designer <br />
-							Front End Developer
-						</p>
-						<div class="row mx-6 mb-3">
-							<div class="col text-center">
-								<a href="#">
-									<i style="font-size: 2rem" class="m-1 bi bi-facebook"></i>
-								</a>
-							</div>
-
-							<div class="col text-center">
-								<a href="#">
-									<i style="font-size: 2rem" class="m-1 bi bi-github"></i>
-								</a>
-							</div>
-
-							<div class="col text-center">
-								<a href="https://www.linkedin.com/in/mabel-mcdonald-020350180/">
-									<i style="font-size: 2rem" class="m-1 bi bi-linkedin"></i>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                            <div class="col text-center">
+                                <a
+                                    href="https://www.linkedin.com/in/mabel-mcdonald-020350180/"
+                                >
+                                    <i
+                                        style="font-size: 2rem"
+                                        class="m-1 bi bi-linkedin"
+                                    ></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium magnam saepe, pariatur et omnis minus quod dolor rem
+                aut aspernatur?
+            </p>
+        </div>
+    </div>
 </x-app>
