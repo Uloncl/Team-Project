4766cca5338fb274d984eacfc09387ce65f200ca
aryanprince <aryanprincesanty@icloud.com>
2022-04-29 12:12:59 +0100

    Improved footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,119 @@
-<footer class="container py-5">
-  <div class="row">
-    <div class="col-12 col-md">
-      <small class="d-block mb-3 text-muted">© 2022</small>
-    </div>
+<!-- Footer -->
+<footer class="text-center text-lg-start text-black bg-sahara">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-between p-4 bg-sahara"></section>
+    <!-- Section: Social media -->
 
-    <div class="row">
-      <div class="col-6 col-md">
-        <h5>About</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="link-secondary" href="about">Team</a></li>
-          <li><a class="link-secondary" href="#">Locations</a></li>
-          <li><a class="link-secondary" href="privacy">Privacy</a></li>
-        </ul>
-      </div>
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold">Sahara</h6>
+                    <hr
+                        class="mb-4 mt-0 d-inline-block mx-auto"
+                        style="
+                            width: 60px;
+                            background-color: #7c4dff;
+                            height: 2px;
+                        "
+                    />
+                    <p>
+                        Find the best games and deals.
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold">Products</h6>
+                    <hr
+                        class="mb-4 mt-0 d-inline-block mx-auto"
+                        style="
+                            width: 60px;
+                            background-color: #7c4dff;
+                            height: 2px;
+                        "
+                    />
+                    <p>
+                        <a href="#!" class="text-white">Games</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-white">Components</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-white">Consoles</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-white">Computers</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold">Useful links</h6>
+                    <hr
+                        class="mb-4 mt-0 d-inline-block mx-auto"
+                        style="
+                            width: 60px;
+                            background-color: #7c4dff;
+                            height: 2px;
+                        "
+                    />
+                    <p>
+                        <a href="#!" class="text-white">About us</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-white">Privacy policy</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-white">Customer care</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold">Contact</h6>
+                    <hr
+                        class="mb-4 mt-0 d-inline-block mx-auto"
+                        style="
+                            width: 60px;
+                            background-color: #7c4dff;
+                            height: 2px;
+                        "
+                    />
+                    <p>
+                        <i class="fas fa-home mr-3"></i> Leeds Beckett University,
+                        Headingley
+                    </p>
+                    <p>
+                        <i class="fas fa-envelope mr-3"></i>
+                        sahara.helpdesk1@gmail.com
+                    </p>
+                    <p><i class="fas fa-phone mr-3"></i> 0113 812 0000</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2020 Copyright:
+        <a class="text-white" href="https://mdbootstrap.com/"
+            >MDBootstrap.com</a
+        >
     </div>
+    <!-- Copyright -->
 </footer>
+<!-- Footer -->
