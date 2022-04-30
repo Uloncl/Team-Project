4ef68caea6ad0fbe7d7c39737895a23e1d3e584c
aryanprince <aryanprincesanty@icloud.com>
2022-04-30 22:01:10 +0100

    Added MDB, Google Fonts, FA CDN

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -13,7 +13,27 @@
 		<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+		<!-- Font Awesome -->
+		<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+		rel="stylesheet"
+		/>
+		<!-- Google Fonts -->
+		<link
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+		rel="stylesheet"
+		/>
+		<!-- MDB -->
+		<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css"
+		rel="stylesheet"
+		/>
 		<script src="https://kit.fontawesome.com/3b3d7324d5.js" crossorigin="anonymous"></script>
+		<!-- MDB -->
+		<script
+		type="text/javascript"
+		src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js"
+		></script>
 
 	</head>
 	<body class="bg-light" style="overflow-x:hidden; min-width: 1880px; width: auto;">
